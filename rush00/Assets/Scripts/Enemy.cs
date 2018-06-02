10e52bf9222391893138aac1423c73b6d8a26424
Clement CHAMEYRAT <cchameyr@e2r11p19.42.fr>
2018-06-02 19:51:50 +0200

    auto save

--- rush00/Assets/Scripts/Enemy.cs
+++ rush00/Assets/Scripts/Enemy.cs
@@ -7,49 +7,57 @@ public class Enemy : MonoBehaviour {
 	private Vector3 _target;
 	private bool _alive;
 	private bool _alerted;
+
+
 	[SerializeField] private Checkpoint _checkpoint;
 	[SerializeField] private float speed;
 	// [SerializeField] private Weapon _weapon;
 
 	public GameObject sprites;
-	
+	public GameObject leg;
+	public bool moving;
+
 
 	void Start () {
 		_target = transform.position;
 		_alerted = false;
+		if (moving) {
+			leg.GetComponent<Animator> ().Play ("legMoving");
+		}
 	}
-	
+
 	void FixedUpdate () {
-		HandleDirection();
-		
+		HandleDirection ();
+
 		if (!_alerted && _checkpoint != null) {
 			if (_target == transform.position) {
 				_checkpoint = _checkpoint.nextCheckpoint;
 			}
-			_target = _checkpoint.transform.position;			
-		} 
+			_target = _checkpoint.transform.position;
+		}
 		// else if (_alerted && _target == transform.position) {
 		// 	_target = new Vector3(Random.Range(transform.position.x -2, transform.position.x + 2), Random.Range(transform.position.y -2, transform.position.y + 2));
 		// }
 
-		transform.position = Vector3.MoveTowards(transform.position, _target, speed * Time.deltaTime);
+		transform.position = Vector3.MoveTowards (transform.position, _target, speed * Time.deltaTime);
 	}
 
-	void OnCollisionEnter2D(Collision2D other) {
+	void OnCollisionEnter2D (Collision2D other) {
+
 	}
 
-	void OnTriggerExit2D(Collider2D other) {
+	void OnTriggerExit2D (Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			if (_alerted) {
-				print("ARRETE");
+				print ("ARRETE");
 				_alerted = false;
 				// StartCoroutine("StopFollowPlayer");
 			}
 		}
 	}
 
-	void OnTriggerStay2D(Collider2D other) {
-		transform.rotation = Quaternion.Euler(0, 0, 0);
+	void OnTriggerStay2D (Collider2D other) {
+		transform.rotation = Quaternion.Euler (0, 0, 0);
 
 		if (other.gameObject.tag == "Player") {
 			// TODO passer les portes !
@@ -59,26 +67,26 @@ public class Enemy : MonoBehaviour {
 			} else {
 
 				Vector3 dir = (playerPos - transform.position).normalized;
-				RaycastHit2D hit = Physics2D.Raycast(transform.localPosition, dir, Mathf.Infinity, LayerMask.GetMask ("Player", "Wall"));
+				RaycastHit2D hit = Physics2D.Raycast (transform.localPosition, dir, Mathf.Infinity, LayerMask.GetMask ("Player", "Wall"));
 				if (hit) {
 					if (hit.collider.gameObject.layer == 11) {
 						_alerted = true;
 						_target = playerPos;
-					} 
+					}
 				}
 			}
 		}
 	}
 
-	private void HandleDirection() {
+	private void HandleDirection () {
 		Vector3 difference = _target - transform.position;
 		difference.Normalize ();
 		float rotation_z = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
 		sprites.transform.rotation = Quaternion.Euler (0f, 0f, rotation_z + 90);
 	}
 
-	private IEnumerator StopFollowPlayer() {
-		yield return new WaitForSeconds(2);
+	private IEnumerator StopFollowPlayer () {
+		yield return new WaitForSeconds (2);
 		_alerted = false;
 	}
 }
